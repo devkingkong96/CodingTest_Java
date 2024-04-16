26e5a6e80218825096592f167de1b2d8edb883ab
김선진 <148295592+devkingkong96@users.noreply.github.com>
2024-04-16 21:13:58 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        String [] arr = {"a","e","i","o","u"};
+        for(String s : arr) my_string=my_string.replace(s,"");
+        return my_string;
+    }
+}
